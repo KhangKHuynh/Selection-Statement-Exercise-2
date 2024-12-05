82d8df96bf1709cb4d152b7659d8a63991984a44
Khang Huynh <kkyhuynh@gmail.com>
2024-12-05 11:36:23 -0600

    selection exercise #2

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,37 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            FavSubject();
+        }
+
+        public static void FavSubject()
+        {
+            Console.WriteLine("What is your favorite school subject?");
+            string subject  = Console.ReadLine();
+
+            switch (subject.ToLower())
+            {
+                case "math":
+                    Console.WriteLine("Math is trash");
+                    break;
+                case "science":
+                    Console.WriteLine("The mitochondria is the powerhouse of the cell");
+                    break;
+                case "pe":
+                case "p.e":
+                case "physical education":
+                    Console.WriteLine("Get your sweat on");
+                    break;
+                case "history":
+                    Console.WriteLine("studious legend");
+                    break;
+                case "geography":
+                    Console.WriteLine("Learning about other countries like Djibouti was so fun");
+                    break;
+                default:
+                    Console.WriteLine("That's awesome!");
+                    break;
+            }
         }
     }
 }
